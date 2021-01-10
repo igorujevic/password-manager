e04b976af816948939f654fbc304baf3583cb8a6
igorujevic <iujevi00@fesb.hr>
2021-01-10 16:00:23 +0100

    Add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  pages: {
+    index: {
+      entry: path.join(__dirname, 'client/main.js'),
+      favicon: path.join(__dirname, 'client/assets/favicon.ico')
+    }
+  },
+  configureWebpack: ({
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'client')
+      },
+      extensions: ['.js', '.vue', '.json']
+    },
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000'
+        }
+      }
+    },
+    devtool: 'source-map'
+  })
+};
