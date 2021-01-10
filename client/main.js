9b995a2312e929b52c3a055e3cada64d78ac6061
igorujevic <iujevi00@fesb.hr>
2021-01-10 16:11:18 +0100

    Add vue-router

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import App from './App.vue';
+import router from './router';
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app');
