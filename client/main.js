b2c73d3794e82a4e117e6a8ebc90989f5c0bf3dd
igorujevic <iujevi00@fesb.hr>
2021-01-10 16:17:17 +0100

    Add init store (no modules)

--- client/main.js
+++ client/main.js
@@ -1,10 +1,12 @@
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import Vue from 'vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app');
