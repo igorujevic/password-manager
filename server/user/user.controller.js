1d72877c17c89277fc0f0480e71c28dfcd447bb6
igorujevic <iujevi00@fesb.hr>
2021-03-20 11:41:50 +0100

    Fix user controller - check if admin getAll users - fix other responses

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -13,7 +13,7 @@ async function register(req, res) {
   // Check if this user already exisits
   const user = await User.findOne({ email });
 
-  if (user) return res.status(201).json({ message: 'Unabale to register with this mail. Try another one.' });
+  if (user) return res.status(201).json({ sucess: false, message: 'Unabale to register with this mail. Try another one.' });
 
   const salt = await bcrypt.genSalt(round);
 
@@ -21,12 +21,13 @@ async function register(req, res) {
     username,
     email,
     password: await bcrypt.hash(password, salt),
-    admin: true,
+    admin: false,
     active: false,
     createdAt: Date.now()
   });
   await newUser.save();
   return res.status(200).json({
+    sucess: true,
     message: 'User registered',
     newUser
   });
@@ -36,9 +37,9 @@ async function login(req, res) {
   const { email, password } = req.body;
   console.log(email, password);
   const user = await User.findOne({ email });
-  if (!user) return res.status(401).json({ message: 'Auth failed!' });
+  if (!user) return res.status(401).json({ success: false, message: 'Auth failed!' });
   const status = await bcrypt.compare(password, user.password);
-  if (!status) return res.status(401).json({ message: 'Auth failed!' });
+  if (!status) return res.status(401).json({ sucess: false, message: 'Auth failed!' });
   const token = jwt.sign(
     {
       id: user._id,
@@ -50,14 +51,24 @@ async function login(req, res) {
     }
   );
   return res.status(200).json({
+    sucess: true,
     message: 'Auth successful!',
     token
   });
 }
 
 async function getAll(req, res) {
+  // get token from header
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) return res.status(403).send({ success: false, message: 'Forbidden' });
+  // decode token
+  const decoded = jwt.decode(token, AUTH_JWT_SECRET);
+  // find that user
+  const user = await User.findOne({ email: decoded.email });
+  // check if admin
+  if (!user.admin) return res.status(403).send({ success: false, message: 'Forbidden' });
   const users = await User.find();
-  return res.send({ users });
+  return res.send({ success: true, users });
 }
 
 module.exports = {
