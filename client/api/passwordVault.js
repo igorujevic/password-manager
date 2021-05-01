c00bd069ceb6acbef7e0122340a4a15710960cac
igorujevic <iujevi00@fesb.hr>
2021-05-01 21:10:55 +0200

    Add getAll pv api call

--- client/api/passwordVault.js
+++ client/api/passwordVault.js
@@ -3,13 +3,19 @@ import axios from 'axios';
 const baseURL = process.env.VUE_APP_API_PATH;
 
 const url = {
-  create: '/password-vault/create'
+  create: '/password-vault/create',
+  getAll: '/password-vault'
 };
 
 function create(data, params) {
   return axios.post(`http://localhost:3000${baseURL}${url.create}`, data, params);
 }
 
+function getAll(params) {
+  return axios.get(`http://localhost:3000${baseURL}${url.getAll}`, params);
+}
+
 export default {
-  create
+  create,
+  getAll
 };
